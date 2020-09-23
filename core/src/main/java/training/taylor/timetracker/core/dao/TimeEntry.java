30337ce1c42244b549d0975a195cc59c2445936e
nubang7 <42139382+nubang7@users.noreply.github.com>
2020-09-23 15:13:35 +0900

    Update TimeEntry.java

--- core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
+++ core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
@@ -13,30 +13,51 @@ public class TimeEntry {
     private float rate;
     private int time;
 
+    /**
+     * Created by Jason on 6/19/2015.
+     */
     public final String getDescription() {
         return description;
     }
-
+    
+    /**
+     * Created by Jason on 6/19/2015.
+     */
     public final void setDescription(final String value) {
         this.description = value;
     }
-
+    
+    /**
+     * Created by Jason on 6/19/2015.
+     */
     public final float getRate() {
         return rate;
     }
     
+    /**
+     * Created by Jason on 6/19/2015.
+     */
     public final void setRate(float value) {
         this.rate = value;
     }
 
+    /**
+     * Created by Jason on 6/19/2015.
+     */    
     public final int getTime() {
         return time;
     }
 
+    /**
+     * Created by Jason on 6/19/2015.
+     */    
     public final void setTime(int value) {
         this.time = value;
     }
 
+    /**
+     * Created by Jason on 6/19/2015.
+     */
     @Override
     public final String toString() {
         return "TimeEntry" ;
