e2ef73d42581b27f89d822c5241290f3fb78578c
nubang7 <42139382+nubang7@users.noreply.github.com>
2020-09-23 14:58:54 +0900

    Update TrackerCoreConfig.java

--- core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
+++ core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
@@ -15,6 +15,18 @@ import java.util.List;
 @ComponentScan("training.taylor.timetracker.core")
 public class TrackerCoreConfig {
 
+        /**
+     * Converts an array to a Collection.
+     *
+     * <p>First, creates a new Collection of the requested target type.
+     * Then adds each array element to the target collection.
+     * Will perform an element conversion from the source component type
+     * to the collection's parameterized type if necessary.
+     *
+     * @author Keith Donald
+     * @author Juergen Hoeller
+     * @since 3.0
+     */
     @Bean(name = "timesheet")
     public List<TimeEntry> timeEntries() {
         return new ArrayList<>();
