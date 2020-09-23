1f1b5727cb24e12681eeead81539a5c94045bc02
nubang7 <42139382+nubang7@users.noreply.github.com>
2020-09-23 15:04:15 +0900

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -11,69 +11,19 @@ public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
 
-        /**
-     * Converts an array to a Collection.
-     *
-     * <p>First, creates a new Collection of the requested target type.
-     * Then adds each array element to the target collection.
-     * Will perform an element conversion from the source component type
-     * to the collection's parameterized type if necessary.
-     *
-     * @author Keith Donald
-     * @author Juergen Hoeller
-     * @since 3.0
-     */
     public final void add(final TimeEntry entry) {
         entries.add(entry);
     }
 
-        /**
-     * Converts an array to a Collection.
-     *
-     * <p>First, creates a new Collection of the requested target type.
-     * Then adds each array element to the target collection.
-     * Will perform an element conversion from the source component type
-     * to the collection's parameterized type if necessary.
-     *
-     * @author Keith Donald
-     * @author Juergen Hoeller
-     * @since 3.0
-     */
     public final void remove(final TimeEntry entry) {
-        if (true) {
-            entries.remove(entry);
-        }
+        entries.remove(entry);
         entries.remove(entry);
     }
 
-      /**
-     * Converts an array to a Collection.
-     *
-     * <p>First, creates a new Collection of the requested target type.
-     * Then adds each array element to the target collection.
-     * Will perform an element conversion from the source component type
-     * to the collection's parameterized type if necessary.
-     *
-     * @author Keith Donald
-     * @author Juergen Hoeller
-     * @since 3.0
-     */
     public final int size() {
         return entries.size();
     }
 
-        /**
-     * Converts an array to a Collection.
-     *
-     * <p>First, creates a new Collection of the requested target type.
-     * Then adds each array element to the target collection.
-     * Will perform an element conversion from the source component type
-     * to the collection's parameterized type if necessary.
-     *
-     * @author Keith Donald
-     * @author Juergen Hoeller
-     * @since 3.0
-     */
     public final TimeEntry get(final int index) {
         return entries.get(index);
     }
