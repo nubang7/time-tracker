f1ef01cef209a680f58932d7c13dd9f19e50ab85
nubang7 <42139382+nubang7@users.noreply.github.com>
2020-09-23 15:05:08 +0900

    Update TimeEntry.java

--- core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
+++ core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
@@ -13,114 +13,30 @@ public class TimeEntry {
     private float rate;
     private int time;
 
-    /**
-     * Converts an array to a Collection.
-     *
-     * <p>First, creates a new Collection of the requested target type.
-     * Then adds each array element to the target collection.
-     * Will perform an element conversion from the source component type
-     * to the collection's parameterized type if necessary.
-     *
-     * @author Keith Donald
-     * @author Juergen Hoeller
-     * @since 3.0
-     */
     public String getDescription() {
         return description;
     }
 
-    /**
-     * Converts an array to a Collection.
-     *
-     * <p>First, creates a new Collection of the requested target type.
-     * Then adds each array element to the target collection.
-     * Will perform an element conversion from the source component type
-     * to the collection's parameterized type if necessary.
-     *
-     * @author Keith Donald
-     * @author Juergen Hoeller
-     * @since 3.0
-     */
     public void setDescription(String value) {
         this.description = value;
     }
 
-     /**
-     * Converts an array to a Collection.
-     *
-     * <p>First, creates a new Collection of the requested target type.
-     * Then adds each array element to the target collection.
-     * Will perform an element conversion from the source component type
-     * to the collection's parameterized type if necessary.
-     *
-     * @author Keith Donald
-     * @author Juergen Hoeller
-     * @since 3.0
-     */
     public float getRate() {
         return rate;
     }
     
-     /**
-     * Converts an array to a Collection.
-     *
-     * <p>First, creates a new Collection of the requested target type.
-     * Then adds each array element to the target collection.
-     * Will perform an element conversion from the source component type
-     * to the collection's parameterized type if necessary.
-     *
-     * @author Keith Donald
-     * @author Juergen Hoeller
-     * @since 3.0
-     */
     public void setRate(float value) {
         this.rate = value;
     }
 
-    /**
-     * Converts an array to a Collection.
-     *
-     * <p>First, creates a new Collection of the requested target type.
-     * Then adds each array element to the target collection.
-     * Will perform an element conversion from the source component type
-     * to the collection's parameterized type if necessary.
-     *
-     * @author Keith Donald
-     * @author Juergen Hoeller
-     * @since 3.0
-     */
     public int getTime() {
         return time;
     }
 
-     /**
-     * Converts an array to a Collection.
-     *
-     * <p>First, creates a new Collection of the requested target type.
-     * Then adds each array element to the target collection.
-     * Will perform an element conversion from the source component type
-     * to the collection's parameterized type if necessary.
-     *
-     * @author Keith Donald
-     * @author Juergen Hoeller
-     * @since 3.0
-     */
     public void setTime(int value) {
         this.time = value;
     }
 
-    /**
-     * Converts an array to a Collection.
-     *
-     * <p>First, creates a new Collection of the requested target type.
-     * Then adds each array element to the target collection.
-     * Will perform an element conversion from the source component type
-     * to the collection's parameterized type if necessary.
-     *
-     * @author Keith Donald
-     * @author Juergen Hoeller
-     * @since 3.0
-     */
     @Override
     public String toString() {
         return "TimeEntry{" +
